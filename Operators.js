6d897267d4c051b4a804ce4ef13d516c2cc709e7
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2026-03-18 16:38:50 +0200

    Arithmetic Operators

---
+++ Operators.js
@@ -0,0 +1,13 @@
+function Operators() {
+    let num1 = 23;
+    let num2 = 43;
+    let Sub = num1 - num2 ;
+    let Sum = num1 + num2;
+    let Multi = num1 * num2;
+    let Div = num2 / num1 ;
+    console.log("The substraction is :"+ Sub);
+    console.log("The Sum of two numbers is:"+Sum);
+    console.log("The Multiplication is :"+Multi);
+    console.log("The Division is :"+ Div);
+}
+Operators();
