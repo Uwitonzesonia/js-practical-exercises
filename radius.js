dada182ad3088be963320fc3453d085849e2916c
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2026-03-18 16:26:06 +0200

    second program in the gym

--- radius.js
+++ radius.js
@@ -1,1 +1,8 @@
-function 
+function radius(){
+    let num = 7;
+    const Pi = 3.14;
+    let AreaCicle = num * num * Pi;
+    console.log("The array of the Cicle is:"+AreaCicle);
+    
+}
+radius();
