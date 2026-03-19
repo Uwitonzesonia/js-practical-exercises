7979d8344d2a6bb9931ba97b6e9dfbcb9778e139
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2026-03-19 17:27:17 +0200

    Escaping String with adding more Special charcter

--- EscapingString.js
+++ EscapingString.js
@@ -2,5 +2,6 @@ function EscapingString(){
     console.log("Hello \tWorld");
     console.log("Escaping \n String");
     console.log("We are going to \" study. we have lecture uhora arakaye yazamuye amazuru\" to day we are going to present but I know is going to came we ugly !!!");
+    console.log("Up up\n\tdown down");
 }
 EscapingString();
