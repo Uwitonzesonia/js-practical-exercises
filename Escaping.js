2c2508b89b8114d119822e0c12749109472136ea
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2026-03-23 13:04:53 +0200

    Escaping story

---
+++ Escaping.js
@@ -0,0 +1,9 @@
+function Escaping(){
+    console.log("He is ,\" Hand some\"");
+    console.log('I\'m happy');
+    console.log("she is so intrested\n I Really like her");
+    console.log(" This is a Good friend i found\\");
+    console.log("Call\\ Her\\ Hope batamuriza Kindest person");
+
+}
+Escaping ();
