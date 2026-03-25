018d145138042a4695dc165c26f60202bb8c23e5
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2026-03-25 13:13:16 +0200

    days of week

---
+++ daysofweek.js
@@ -0,0 +1,27 @@
+function daysofweek () {
+  
+  let day = 4;
+  if (day ==1){
+    console.log("the day is monday");
+   }
+  else if ( day == 2){
+    console.log("the day is tuesday");
+  }
+  else if ( day == 3){
+    console.log("the day is wensday")
+  }
+  else if ( day == 4){
+    console.log("the day is thursday")
+  }
+else if ( day == 5){
+  console.log("the day is friday")
+}
+  else if (day == 6){
+    console.log("the day is saturday")
+  }
+  else {
+    console.log("the day is sunday")
+  }
+
+}
+daysofweek();
