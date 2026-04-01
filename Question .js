786321056ceac27d30db4623dcaed08c0d5b28c2
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2026-04-01 16:52:27 +0200

    Arrays

--- Question .js
+++ Question .js
@@ -24,16 +24,24 @@
 //     console.log("Welcome in stressful university of AUCA");
 // }
 // callback(greetings);
-function Array(){
-    let names = ["sonia","kaneza","nshuti","Paul"];
-    let Ages = [21,8,19,30];
-    names[0]="Uwitonze sonia";
-    names[1]="Kaneza fabiola";
-    names[2]="Nshuti Everson";
-    names[3]="Halerimana Paul";
+// function Array(){
+//     let names = ["sonia","kaneza","nshuti","Paul"];
+//     let Ages = [21,8,19,30];
+//     names[0]="Uwitonze sonia";
+//     names[1]="Kaneza fabiola";
+//     names[2]="Nshuti Everson";
+//     names[3]="Halerimana Paul";
 
-    console.log(names);
-    console.log(Ages);
-    console.log(names.length);
+//     console.log(names);
+//     console.log(Ages);
+//     console.log(names.length);
+// }
+// Array();
+function Looping(){
+let names = ["Sonia","John","Bella","smith"];
+for(i=0; i<names.length; i++){
+    console.log(names[i]);
 }
-Array();
+
+}
+Looping();
