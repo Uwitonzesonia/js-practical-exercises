70ba7e02588009d2b060c5e6c36e4b2e04e9e176
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2026-04-01 16:42:56 +0200

    scope function

--- Question .js
+++ Question .js
@@ -8,11 +8,32 @@
 
 
 // Greeting();
-function greeting(){
-    let Students = true;
-    if (true){
-         var Name = " students in AUCA";
-    }
-    console.log(Name);
+// function greeting(){
+//     let Students = true;
+//     if (true){
+//          var Name = " students in AUCA";
+//     }
+//     console.log(Name);
+// }
+// greeting();
+// function callback(greet){
+//     console.log("Hey ! every one");
+//     greet();
+// }
+// function greetings(){
+//     console.log("Welcome in stressful university of AUCA");
+// }
+// callback(greetings);
+function Array(){
+    let names = ["sonia","kaneza","nshuti","Paul"];
+    let Ages = [21,8,19,30];
+    names[0]="Uwitonze sonia";
+    names[1]="Kaneza fabiola";
+    names[2]="Nshuti Everson";
+    names[3]="Halerimana Paul";
+
+    console.log(names);
+    console.log(Ages);
+    console.log(names.length);
 }
-greeting();
+Array();
